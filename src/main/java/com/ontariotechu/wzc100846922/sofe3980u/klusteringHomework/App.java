b16dba16bc83921894bd379261cfecd399382d99
wzc-OntarioTechU <william.chamberlain@ontariotechu.net>
2024-03-12 17:09:02 -0400

    Inital Commit with empty maven project.

---
+++ src/main/java/com/ontariotechu/wzc100846922/sofe3980u/klusteringHomework/App.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.wzc100846922.sofe3980u.klusteringHomework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ontariotechu/wzc100846922/sofe3980u/klusteringHomework/AppTest.java
@@ -0,0 +1,20 @@
+package com.ontariotechu.wzc100846922.sofe3980u.klusteringHomework;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
